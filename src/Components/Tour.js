c6b969792ba91dfa4864cc0326ddfd9eafef30f2
Uros Drca <urosdrca22@gmail.comgit config --global user.name Uros>
2022-02-22 15:47:47 +0100

    Added new functionalities

--- src/Components/Tour.js
+++ src/Components/Tour.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, Button } from 'react-bootstrap'
 import './Tour.css'
 
 const Tour = ({ id, name, info, image, price }) => {
+    const [readMore, setReadMore] = useState(false);
+
     return(
         <Card className="text-center mb-3 shadow p-3" style={{ width: '40rem'}}>
             <Card.Img className="tour_image" src={image}></Card.Img>
             <Card.Body>
                 <Card.Header as="h5">{name}</Card.Header>
-                <Card.Text>{info}</Card.Text>
+                <Card.Text>{readMore ? info : `${info.substring(0, 200)}...`}
+                <button onClick={() => setReadMore(!readMore)}>
+                    {readMore ? 'Show less' : 'Read more'}
+                </button>
+                </Card.Text>
                 <Card.Text>${price}</Card.Text>
-                <Button>Remove Tour</Button>
+                <Button>Remove</Button>
             </Card.Body>
         </Card>
     )
